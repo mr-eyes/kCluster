bb0f54806c60264075516a66e7d6dbc003c33065
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2018-10-29 21:29:55 +0200

    prepare fasta file for kprocessor indexing

---
+++ scripts/kProcessor_prepare.py
@@ -0,0 +1,14 @@
+from Bio import SeqIO
+import sys
+input_file = sys.argv[1]
+output_file = "min"+ "_" + input_file
+names_file = "min" + "_" + input_file + ".names"
+fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
+names = open(names_file,"w")
+with open(output_file,"w") as handle:
+    for fasta in fasta_sequences:
+        description = fasta.description.split("|")[0]
+        names.write(description + "\t" + description + "\n")
+        fasta.description = fasta.id = fasta.name = description
+        SeqIO.write(fasta, handle, "fasta")
+names.close()
