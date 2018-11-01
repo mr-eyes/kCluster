33397fc8c97b010d0e954987a4a34d911b2a7161
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2018-11-01 22:04:49 +0200

    Prepare from different dir

--- scripts/kProcessor_prepare.py
+++ scripts/kProcessor_prepare.py
@@ -1,8 +1,9 @@
 from Bio import SeqIO
 import sys
+import os
 input_file = sys.argv[1]
-output_file = "min"+ "_" + input_file
-names_file = "min" + "_" + input_file + ".names"
+output_file = "min" + "_" + os.path.basename(input_file)
+names_file = "min" + "_" + os.path.basename(input_file) + ".names"
 fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
 names = open(names_file,"w")
 with open(output_file,"w") as handle:
