54becae760047ded3d639c881844f8f4c149845c
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-03-27 03:44:12 +0200

    dual verification with traversing the index

--- scripts/tests/test_pairwise.sh
+++ scripts/tests/test_pairwise.sh
@@ -1,4 +1,4 @@
-set -euo pipefail
+#set -euo pipefail
 
 RUNLOG=runlog.log
 echo "Run by `whoami` on `date`" > $RUNLOG # write log while running.
@@ -17,12 +17,33 @@ do
     # Get genes header
     gene1header=$(grep -E "\s${gene1}$" ${PREFIX}.namesMap | awk '{print $1}')
     gene2header=$(grep -E "\s${gene2}$" ${PREFIX}.namesMap | awk '{print $1}')
+    echo $gene1header
+    echo $gene2header
+    echo "-------------------------------"
+    
+    READS1=$(grep "${gene1header}$" ${REF_NAMES} | awk '{print $1}')
+    READS2=$(grep "${gene2header}$" ${REF_NAMES} | awk '{print $1}')
+
+    echo "" > tmp1.fa
+    echo "" > tmp2.fa
+
+    for value in $READS1
+    do
+        seqkit grep -r -p ${value} ${REF_FA} >> tmp1.fa
+    done
+
+    for value in $READS2
+    do
+        seqkit grep -r -p ${value} ${REF_FA} >> tmp2.fa
+    done
+    
     
     # Count shared kmers
-    seqkit grep -r -p ${gene1header} ${REF_FA} | jellyfish count -t 8 -C -m 21 -s 1000 -o tmp_jelly_seq1 /dev/fd/0
-    seqkit grep -r -p ${gene2header} ${REF_FA} | jellyfish count -t 8 -C -m 21 -s 1000 -o tmp_jelly_seq2 /dev/fd/0
+    seqkit seq tmp1.fa | jellyfish count -t 8 -C -m 21 -s 1000 -o tmp_jelly_seq1 /dev/fd/0
+    seqkit seq tmp2.fa  | jellyfish count -t 8 -C -m 21 -s 1000 -o tmp_jelly_seq2 /dev/fd/0
     jellyfish dump -t tmp_jelly_seq1 | grep -E "^[^>]" | sort > tmp_kmers_1
     jellyfish dump -t tmp_jelly_seq2 | grep -E "^[^>]" | sort > tmp_kmers_2
+
     verified_kmers=$(join tmp_kmers_1 tmp_kmers_2 | wc -l)
     gene1_kmers=$(cat tmp_kmers_1 | wc -l)
     gene2_kmers=$(cat tmp_kmers_2 | wc -l)
@@ -35,6 +56,26 @@ do
         echo "WRONG shared_kmers $gene1header & $gene2header = $verified_kmers not $kmers" >> ${RUNLOG}
     fi
 
+    # Check by traversing the index itself
+    grep "[-,\,]$gene1," ${PREFIX}.map | awk -F'-' '{print $1}' | sort | uniq > seq1_colors
+    grep "[-,\,]$gene2," ${PREFIX}.map | awk -F'-' '{print $1}' | sort | uniq > seq2_colors
+    common_colors=$(join seq1_colors seq2_colors)
+    rm seq1_colors seq2_colors
+
+    shared_kmers=0
+
+    for value in $common_colors
+    do
+        cnt=$(grep ":${value}$" tmp_idx/*map | wc -l)
+        shared_kmers=$((shared_kmers+cnt))
+    done
+
+    if [ $shared_kmers -eq $verified_kmers ];
+    then
+        echo "CORRECT [2] shared_kmers $gene1header & $gene2header = $verified_kmers" >> ${RUNLOG}
+    else
+        echo "WRONG [2] shared_kmers $gene1header & $gene2header = $verified_kmers not $shared_kmers" >> ${RUNLOG}
+    fi
 
     # Verify similarity percentage kmers
     : '
@@ -59,4 +100,4 @@ do
     echo "~~~~~" >> ${RUNLOG}
 done
 
-rm tmp_jelly_seq1 tmp_jelly_seq2 tmp_kmers_1 tmp_kmers_2
+rm tmp_jelly_seq1 tmp_jelly_seq2 tmp_kmers_1 tmp_kmers_2 tmp1.fa tmp2.fa
