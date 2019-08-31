e4579c82c14d59e21e8320f046457c4d580f460b
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-09-01 00:00:21 +0200

    refactor to allow TSV clustering

--- src/cluster/clustering.py
+++ src/cluster/clustering.py
@@ -14,41 +14,51 @@ class kClusters:
     names_map = dict()
     components = defaultdict(set)
 
-    def __init__(self,logger_obj, sqlite_file, userQs, cut_off_threshold):
-        self.Logger = logger_obj
-        
+    def sqlite_constructor(self, sqlite_file, userQs):
         try:
             self.conn = sqlite3.connect(sqlite_file)
-        
+
         except sqlite3.Error as err:
             self.Logger.ERROR(f"couldn't connect to {sqlite_file}\n{err}")
 
         # Check user selected Qs
         sqliteQs = [int(Q.split("_")[1]) for Q in self.sqlite_getQs()]
-        
+
         if userQs == [-1]:
             self.userQs = sqliteQs
         else:
             commonQs = set(userQs).intersection(set(sqliteQs))
             ln_commonQs = len(commonQs)
             ln_userQs = len(userQs)
-            
+
             if not ln_commonQs:
                 self.Logger.ERROR("invalid virtualQs range, none of them found in the sqlite database")
-            
+
             elif ln_commonQs == ln_userQs:
                 self.userQs = userQs
-            
+
             else:
                 unfound = set(userQs) - set(sqliteQs)
-                self.Logger.WARNING(f"The following Qs '{unfound}' couldn't be found in the sqlite database, processing the others..")
+                self.Logger.WARNING(
+                    f"The following Qs '{unfound}' couldn't be found in the sqlite database, processing the others..")
                 self.userQs = list(commonQs)
-            
-        
-        self.cut_off_threshold = cut_off_threshold
+
+
         self.sqlite_file = sqlite_file
         self.kSize = self.conn.execute("SELECT value from meta_info WHERE key='kSize'").fetchone()[0]
         self.sqlite_get_namesmap()
+
+    def tsv_constructor(self, tsv_file, userQs):
+        pass
+
+    def __init__(self,logger_obj, pairwise_file_type, pairwise_file, userQs, cut_off_threshold):
+        self.Logger = logger_obj
+        self.cut_off_threshold = cut_off_threshold
+
+        if pairwise_file_type == "sqlite":
+            self.sqlite_constructor(pairwise_file, userQs)
+        elif pairwise_file_type == "tsv":
+            self.tsv_constructor(pairwise_file, userQs)
 
     def ids_to_names(self, cluster):
         new_cluster = []
@@ -91,7 +101,7 @@ class kClusters:
             min_kmers = row[2]
             similarity = 0.0
             Q_prev = 0
-            
+
             for Q_val, idx in indeces.items():
                 Q_curr = row[idx]
                 sim = ( (Q_curr - Q_prev) * (Q_val / self.kSize) ) / min_kmers
@@ -100,7 +110,7 @@ class kClusters:
 
             if similarity < self.cut_off_threshold:
                 continue
-            
+
             self.source.append(seq1)
             self.target.append(seq2)
 
@@ -125,7 +135,7 @@ class kClusters:
             if lx != ly:
                 registers[lx] = ly
 
-        
+
         for i in range(len(self.source)):
             union(self.source.pop(), self.target.pop())
 
@@ -138,7 +148,7 @@ class kClusters:
         kCluster_file_name = f"clusters_{self.cut_off_threshold:.2f}%_"
         kCluster_file_name += os.path.basename(self.sqlite_file).split(".")[0]
         kCluster_file_name += ".tsv"
-        
+
         with open(kCluster_file_name, 'w') as kClusters:
             kClusters.write("kClust_id\tseqs_ids\n")
             for cluster_id, (k, v) in enumerate(self.components.items(), 1):
@@ -151,24 +161,36 @@ class kClusters:
 @click.option('-m','--min-q', required=False, type=int, default = None, help="minimum virtualQ")
 @click.option('-M','--max-q', required=False, type=int, default = None, help="maximum virtualQ")
 @click.option('-s','--step-q', required=False, type=int, default = None, help="virtualQs range step")
-@click.option('-c','--cutoff', required=False, type=click.FloatRange(0, 1, clamp=False) , default = 0.0, show_default=True, help="cluster sequences with (similarity > cutoff)")
-@click.option('-d', '--db', required=True, type=click.Path(exists=True), help="sqlite database file")
+@click.option('-c','--cutoff', required=False, type=click.FloatRange(0, 1, clamp=False), default = 0.0, show_default=True, help="cluster sequences with (similarity > cutoff)")
+@click.option('-d', '--db', required=False, type=click.Path(exists=True), default=None,  help="sqlite database file")
+@click.option('-t', '--tsv', required=False, type=click.Path(exists=True), default=None, help="sqlite database file")
 @click.pass_context
-def main(ctx, min_q, max_q, step_q, db, cutoff):
+def main(ctx, min_q, max_q, step_q, db, tsv, cutoff):
     """Sequences clustering regarding user-selected virtualQs."""
     scanQs = map(bool, [min_q, max_q, step_q])
 
     if True in scanQs and False in scanQs:
         ctx.obj.ERROR("Please complete the virtualQs range")
-    
+
     elif not(min_q and max_q and step_q):
         ctx.obj.WARNING("processing all virtualQs in the sqlite database")
         userQs = [-1]
-    
+
     else:
         userQs = [Q for Q in range(min_q, max_q + 1, step_q)]
-    
-    kCl = kClusters(logger_obj= ctx.obj, sqlite_file = db, userQs = userQs, cut_off_threshold = cutoff)
+
+    if db == None and tsv:
+        file_type = "tsv"
+    elif tsv == None and db:
+        file_type = "sqlite"
+    elif tsv and db:
+        ctx.obj.ERROR("Can't select both sqlite and tsv.")
+    elif tsv == None and db == None:
+        ctx.obj.ERROR("You must select pairwise matrix for clustering.")
+
+
+    # kCl = kClusters(logger_obj= ctx.obj, sqlite_file = db, userQs = userQs, cut_off_threshold = cutoff)
+    kCl = kClusters(logger_obj=ctx.obj, pairwise_file_type=file_type, pairwise_file = db, userQs = userQs, cut_off_threshold = cutoff)
     kCl.build_graph()
     kCl.clustering()
     kCl.export_kCluster()
